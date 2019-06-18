e718630f8b43214bedd941ae575fe3bb54bfc147
takahirosawamura <cawst222@gmail.com>
2019-06-18 13:53:19 +0900

    admin

--- dashboard/consumption/admin.py
+++ dashboard/consumption/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User_data
+from .models import Consumption
 
-# Register your models here.
+admin.site.register(User_data)
+admin.site.register(Consumption)
